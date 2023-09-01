326777fe67e343e5e13824d3a44890de5c7ef827
sudhakarreddy2000 <32480429+sudhakarreddy2000@users.noreply.github.com>
2023-09-01 10:37:16 +0530

    Add files via upload

---
+++ params.js
@@ -0,0 +1,9 @@
+const express=require('express')
+const app=express()
+
+//in addressbar type localhost:3000/profile/123
+ app.get('/profile/:id',(req,res)=>{
+    res.send('Profile id is '+req.params.id)
+ })
+
+app.listen(3000,()=>console.log('server started..'))
